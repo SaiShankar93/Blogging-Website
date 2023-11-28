126767c3bec4cd387de4b05d806c89ea51e990d8
Sai Shankar <113768646+SaiShankar93@users.noreply.github.com>
2023-11-28 07:28:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -188,5 +188,5 @@ app.get('/delete/:title', async (req, res) => {
 });
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-    console.log(`Server started on port ${port)`);
+    console.log(`Server started on port ${port}`);
 });
